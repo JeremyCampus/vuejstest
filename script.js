560333a4854512b878604e5be872d10d467b0c1a
Jérémy Laurens <jeremy.laurens@Digital-Grenoble.local>
2018-03-02 09:21:33 +0100

    first push

---
+++ script.js
@@ -0,0 +1,32 @@
+
+new Vue({
+    el: '#app1',
+    data: {
+      message: "ಠ_ಠノ"
+    },
+  })
+  
+
+new Vue({
+  el: '#app2',
+  data: {
+    whenReady: '٩(●ᴗ●)۶'
+  },
+
+})
+
+
+  new Vue({
+    el: '#app3',
+    data: {
+      active: false
+    }
+  })
+  
+
+  new Vue({
+    el: '#app4',
+    data: {
+      show: true
+    }
+  })
